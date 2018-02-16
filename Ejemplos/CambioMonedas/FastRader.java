3e90a7c5b91e1d89a82aacc23b93335b6a42386d
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2018-02-16 16:47:19 +0100

    FastReader java

---
+++ Ejemplos/CambioMonedas/FastRader.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+// Imports solo para leer decentemente.
+// Por algo amamos Java para aprender a programar (no).
+
+class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+	br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next(){
+	while (st == null || !st.hasMoreElements()){
+	    try {
+		st = new StringTokenizer(br.readLine());
+	    } catch (IOException e) {
+		System.out.println("Error " + e.getMessage());
+	    }
+	}
+	return st.nextToken();
+    }
+
+    int nextInt(){
+	return Integer.parseInt(next());
+    }
+
+    long nextLong(){
+	return Long.parseLong(next());
+    }
+
+    double nextDouble(){
+	return Double.parseDouble(next());
+    }
+
+    String nextLine(){
+	String str = "";
+	try {
+	    str = br.readLine();
+	}
+	catch (IOException e) {
+	    System.out.println("Error " + e.getMessage());
+	}
+	return str;
+    }
+}
